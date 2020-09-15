23f17b0d2358c59f143da715d1d4edb879d2118a
Anderson Nascimento <agenciablackpearl@gmail.com>
2020-09-15 15:11:10 -0300

    User password update

--- src/api/users/userUpdateService.js
+++ src/api/users/userUpdateService.js
@@ -1,7 +1,25 @@
 const axios = require('axios')
+const bcrypt = require('bcrypt')
+
+const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})/
 
 module.exports = async (req, res, next) => {
     const response = res.locals.bundle
+
+    const password = req.body.password || null
+
+    if(password){
+        if(!password.match(passwordRegex)) {
+            return res.status(400).send({errors: [
+                "Password must be 6-20 characters and numbers. Must have symbols (@#$%) one uppercase letter and one lowercase letter."
+            ]})
+        }
+    
+        const salt = bcrypt.genSaltSync()
+        const passwordHash = bcrypt.hashSync(password, salt)
+        
+        req.body.password = passwordHash
+    }
     
     await axios.get(`https://api.pipedrive.com/v1/persons/search?term=${response.login}&api_token=${process.env.PIPEDRIVE_TOKEN}`)
     .then(async (pipedrive) => {
@@ -9,8 +27,6 @@ module.exports = async (req, res, next) => {
         const persons = await pipedrive.data.data.items.filter( person => person.item.custom_fields.includes(response.login) ).map((person) => { return person.item })
 
         for (const [index, person] of persons.entries()){
-
-            console.log(person.id)
 
             axios.put(`https://api.pipedrive.com/v1/persons/${person.id}?api_token=${process.env.PIPEDRIVE_TOKEN}`, {
                 name: response.name,
@@ -26,6 +42,17 @@ module.exports = async (req, res, next) => {
             
         }
 
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
+    await axios.patch(`https://api.movidesk.com/public/v1/persons?token=${process.env.MOVIDESK_TOKEN}&id=${response.login}`,{
+        businessName: response.name,
+        emails: response.email.map((email) => { return { emailType: 'Profissional', email: email.value, isDefault: email.primary } })
+    })
+    .then(async (response) => {
+        console.log(response)
     })
     .catch(error => {
         console.log(error)
