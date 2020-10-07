84dbc1e65365ecaeddef02e66f60903373098dbd
Anderson Nascimento <agenciablackpearl@gmail.com>
2020-10-06 21:11:35 -0300

    Ticket Service adjustments

--- src/api/helpdesk/newTicketService.js
+++ src/api/helpdesk/newTicketService.js
@@ -1,7 +1,10 @@
 const axios = require('axios')
 
+const Client = require('../clients/clients')
 const User = require('../users/users')
 const LoggingModel = require('../users/logging')
+const Notifications = require('../notifications/notifications')
+const sendGrid = require('../common/sendGrid')
 
 module.exports = async (req, res, next) => {
     const id = req.params.id
@@ -15,6 +18,8 @@ module.exports = async (req, res, next) => {
         if(err) {
             return sendErrorsFromDB(res, err)
         } else if (user) {
+
+            const client = await Client.findOne({reference}).exec()
 
             await axios.get(`https://api.movidesk.com/public/v1/persons?token=${process.env.MOVIDESK_TOKEN}&returnAllProperties=false&id=${user.login}`)
             .then(async (response) => {
@@ -52,10 +57,51 @@ module.exports = async (req, res, next) => {
                 .then(response => {
 
                     if(response.data.id){
+                        Client.findOne({reference})
+
                         LoggingModel.create({
                             user: user._id,
                             action: `Criou um ticket de atendimento: #${response.data.id}`
                         })
+
+                        let msg = []
+
+                        console.log(Notifications.create({
+                            client: client ? client._id : null,
+                            user: user._id,
+                            title: "Novo ticket de atendimento criado",
+                            description: `Ticket de atendimento #${response.data.id}.`,
+                            type: 'ticket',
+                            data: response.data
+                        }))
+
+                        user.email.map(email => { 
+
+                            msg.push({
+                                to: email.value,
+                                templateId: process.env.SENDGRID_TEMPLATE_NEWTICKET,
+                                dynamicTemplateData: {
+                                    id: response.data.id,
+                                    email: email.value,
+                                    type: subject,
+                                    title,
+                                    date: expec_date,
+                                    description
+                                }
+                            })
+
+                        })
+
+                        sendGrid.send(msg,true)
+                        .then(
+                            response => {
+                                if(!response){
+                                    console.log(`New ticket #${response.data.id} notification error, email not sended!`)
+                                }else{
+                                    console.log(`New ticket #${response.data.id} notification sended!`)
+                                }
+                            }
+                        )  
 
                         return res.status(200).json({ id: response.data.id, message: 'Ticket created!' });
                     }else{
@@ -63,13 +109,13 @@ module.exports = async (req, res, next) => {
                     }
                 })
                 .catch(error => {
-                    console.log(error.response.data)
+                    console.log(error)
                     return res.status(400).send({errors: ['Error on create a new ticket!']})
                 })
 
             })
             .catch(error => {
-                console.log(error.response.data)
+                console.log(error)
                 return res.status(400).send({errors: ['It was not possible to create a ticket from this user!']})
             })
 
